f67d32c02c2a8b83171e3cc81706fa2a833bba94
Gabriel Gibeau Sanchez <ggs8922@gmail.com>
2020-03-02 21:42:13 -0500

    ipynb <=> py conversion has backup

--- convert_py_to_ipynb.sh
+++ convert_py_to_ipynb.sh
@@ -1,3 +1,11 @@
-!#/bin/bash
+#!/bin/bash
 
-ipynb-py-convert ./tp2.py1 ./tp2.ipynb
+ipynb_file_name = 'tp2.ipynb'
+
+if [ -e $PWD/$ipynb_file_name ]
+then
+	date_str=$(date +"%Y_%m_%d_%H:%M:%S")
+	cp $PWD/$ipynb_file_name $PWD/'tp2_backup_'$date_str'.ipynb'
+fi
+
+ipynb-py-convert ./tp2.py1 ./$ipynb_file_name
